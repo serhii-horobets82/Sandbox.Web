60ccdc2bac776e4fed60b00d1fcbe818dea4bfab
Sergey <gorserg@hotmail.com>
2019-02-25 01:34:53 +0200

    Move to nav component

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,6 @@
 <template>
   <v-app>
-    <v-toolbar app>
-      <v-toolbar-side-icon>
-        <v-img src="img/icons/favicon-32x32.png"></v-img>
-      </v-toolbar-side-icon>
-      <v-toolbar-title class="headline">
-        <span>EvoFlare</span>
-      </v-toolbar-title>
-
-      <v-spacer></v-spacer>
-
-      <v-toolbar-items class="hidden-sm-and-down">
-        <v-btn flat to="/">Home</v-btn>
-        <v-btn flat to="/evaluation">Evaluation</v-btn>
-        <v-btn flat to="/okr">OKR</v-btn>
-        <v-btn flat to="/about">About</v-btn>
-      </v-toolbar-items>
-
-    </v-toolbar>
-
+    <app-navigation></app-navigation>
     <v-content>
       <router-view/>
     </v-content>
@@ -26,12 +8,12 @@
 </template>
 
 <script>
-  import Nav from '@/components/Nav.vue'
+  import AppNavigation from '@/components/AppNav.vue'
 
   export default {
     name: 'App',
     components: {
-      //Nav
+      AppNavigation
     },
     data () {
       return {
---
+++ src/components/AppNav.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-toolbar app>
+    <v-toolbar-side-icon>
+      <v-img src="img/icons/favicon-32x32.png"></v-img>
+    </v-toolbar-side-icon>
+    <v-toolbar-title class="headline">
+      <span>EvoFlare</span>
+    </v-toolbar-title>
+
+    <v-spacer></v-spacer>
+
+    <v-toolbar-items class="hidden-sm-and-down">
+      <v-btn flat to="/">Home</v-btn>
+      <v-btn flat to="/evaluation">Evaluation</v-btn>
+      <v-btn flat to="/okr">OKR</v-btn>
+      <v-btn flat to="/about">About</v-btn>
+      <v-btn flat>SIGN IN</v-btn>
+    </v-toolbar-items>
+
+  </v-toolbar>
+</template>
+
+<script>
+  export default {
+    name: 'AppNavigation'
+  }
+</script>
+
+<style scoped>
+</style>
