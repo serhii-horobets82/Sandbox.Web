023d7de5e19e1935c700850e7e7c5f02ff962f22
Sergey <gorserg@hotmail.com>
2019-04-12 01:08:09 +0300

    Fix build

--- src/util/index.ts
+++ src/util/index.ts
@@ -2,9 +2,12 @@ const toggleFullScreen = () => {
   let doc = window.document;
   let docEl = doc.documentElement;
 
+  // @ts-ignore
   let requestFullScreen = docEl.requestFullscreen || docEl.mozRequestFullScreen || docEl.webkitRequestFullScreen || docEl.msRequestFullscreen;
+  // @ts-ignore
   let cancelFullScreen = doc.exitFullscreen || doc.mozCancelFullScreen || doc.webkitExitFullscreen || doc.msExitFullscreen;
 
+  // @ts-ignore
   if (!doc.fullscreenElement && !doc.mozFullScreenElement && !doc.webkitFullscreenElement && !doc.msFullscreenElement) {
     requestFullScreen.call(docEl);
   } else {
