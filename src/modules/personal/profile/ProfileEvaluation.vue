e2dfa8502305d57e1f39259983ab48bce6ccda5f
Oleksii Chornousov <oleksii.chornousov@gmail.com>
2019-06-15 13:30:45 +0300

    Profile competences - current-next toggle + data from grades db.

--- src/modules/personal/profile/ProfileEvaluation.vue
+++ src/modules/personal/profile/ProfileEvaluation.vue
@@ -5,11 +5,11 @@
         <!-- <v-card-title>
           Technical Competence
         </v-card-title>-->
-        <v-card-text style="min-height: 150px">
+        <v-card-text style="min-height: 150px; margin-top: 7px;">
           <table class="skillHeaderRow">
             <tbody>
               <tr>
-                <td style="width: 50%">Current competence level</td>
+                <td style="width: 50%">Current level - Junior</td>
                 <td v-for="i in [1,2,3,4,5]" :key="i" class="text-xs-center">{{ `E${i}` }}</td>
               </tr>
             </tbody>
@@ -62,35 +62,57 @@
 
     <v-flex xs6>
       <v-card flat style="background-color: #F7FBFF">
-        <v-card-text style="min-height: 150px">
+        <v-card-text style="min-height: 150px"  v-if="grades">
           <table class="skillHeaderRow">
             <tbody>
               <tr>
-                <td style="width: 50%">Required skills</td>
-                <td v-for="i in [1,2,3,4,5]" :key="i" class="text-xs-center">{{ `E${i}` }}</td>
+                <td style="width: 50%">
+                  <v-btn-toggle v-model="gradeId">
+                    <v-btn flat :value="2" small>
+                      Junior
+                    </v-btn>
+                    <v-btn flat :value="3" small>
+                      Middle
+                    </v-btn>
+                  </v-btn-toggle>
+                </td>
+                <td v-for="i in [1,2,3,4,5]" :key="i"
+                  class="text-xs-center">
+                  {{ `E${i}` }}
+                </td>
               </tr>
             </tbody>
           </table>
-          <table class="skillRow" v-for="competence in competences" :key="competence.id">
+          <table class="skillRow"
+            v-for="competence in grades[gradeId].rows"
+            :key="competence.id">
             <tbody>
               <tr>
-                <td style="width: 50%">{{ competence.name }}</td>
-                <td
-                  v-for="i in [1,2,3,4,5]"
-                  :key="i"
+                <td style="width: 50%">
+                  <v-layout>
+                    {{ competence.name }}
+                    <v-spacer></v-spacer>
+                    <CompetenceInfo :competence="competence"></CompetenceInfo>
+                  </v-layout>
+                </td>
+                <td v-for="i in [1,2,3,4,5]" :key="i"
                   class="competenceLevelFill text-xs-center"
                   style="width: 10%"
                   :class="{
-                    'required': competence.roleLevel === i,
-                    'available': competence.levels[i] && i < competence.roleLevel
+                    'required': competence.competenceLevel === i,
+                    'available': competence.levels[i - 1],// && i < competence.competenceLevel
+                    'selected': i == competence.competenceLevel
                   }"
-                >
-                  <v-icon style="color: #B5D6EC" small v-if="!competence.levels[i]">block</v-icon>
-                  <v-icon
-                    style="color: #B5D6EC"
-                    small
-                    v-if="competence.roleLevel === i"
-                  >error_outlined</v-icon>
+                  >
+                  <v-icon style="color: #B5D6EC" small v-if="!competence.levels[i - 1]">block</v-icon>
+                  <v-tooltip bottom v-if="competence.levels[i - 1] && competence.levels[i - 1].certificates">
+                    <template v-slot:activator="{ on }">
+                      <v-icon
+                        small
+                        v-on="on">assignment</v-icon>
+                    </template>
+                    <span>{{competence.levels[i-1].certificates.map(c=>c.name).join('; ')}}</span>
+                  </v-tooltip>
                 </td>
               </tr>
             </tbody>
@@ -110,7 +132,8 @@ export default {
     EcfCompetenceRow
   },
   data: () => ({
-    competences: []
+    competences: [],
+    grades: null,
   }),
 
   async created() {
@@ -118,6 +141,11 @@ export default {
       this.$backendUrl + `api/employees/profile/ecf-evaluation`
     );
     this.competences = resEcf.data;
+
+    this.gradeId = 2;
+    const roleId = 2;
+    const res = await axios.get(this.$backendUrl + `api/RoleGrades/${roleId}/competences`);
+    this.grades = res.data;
   },
 
   methods: {
@@ -160,37 +188,39 @@ export default {
   margin: 5px;
 }
 
-.competenceLevelFill {
-  border-spacing: 0;
-  // min-width: 22px;
-  // height: 22px;
-  // margin: 1px;
-  // float:left;
+  .competenceLevelFill {
+    border-spacing: 0;
+    // min-width: 22px;
+    // height: 22px;
+    // margin: 1px;
+    // float:left;
 
-  // &.none {
-  //   background-color: lightblue;
-  // }
+    // &.none {
+    //   background-color: lightblue;
+    // }
 
-  &.available,
-  &.required {
-    background-color: rgba(61, 179, 237, 0.15);
-    // background-color: #E3F2FD;
-    // background-color: lightgreen;
-    // cursor: pointer;
+    &.available, &.required {
+      background-color: rgba(61, 179, 237, 0.15);
+      // background-color: #E3F2FD;
+      // background-color: lightgreen;
+      cursor: pointer;
+    }
+
+    &.current {
+      background-color: rgba(61, 179, 237, 0.15);
+      // background-color: green;
+      // cursor: pointer;
+    }
+    &.selected {
+      background-color: #3C88B5;
+    }
+
+    &.roleDesired {
+      border-color: green;
+      border-style: solid;
+      border-width: 2px;
+      padding: 2px;
+      background-clip: content-box;
+    }
   }
-
-  &.current {
-    background-color: rgba(61, 179, 237, 0.15);
-    // background-color: green;
-    // cursor: pointer;
-  }
-
-  &.roleDesired {
-    border-color: green;
-    border-style: solid;
-    border-width: 2px;
-    padding: 2px;
-    background-clip: content-box;
-  }
-}
 </style>
