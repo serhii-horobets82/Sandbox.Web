d6762c185dc421e7fc7d4f29a50b8f4f013489c1
serhii.horobets <serhii.horobets@itera.no>
2019-07-06 01:01:37 +0300

    Salary review

--- src/modules/salary/components/SalaryReview.vue
+++ src/modules/salary/components/SalaryReview.vue
@@ -27,118 +27,115 @@
           </v-card-actions>
         </v-card>
       </v-flex>
-      <v-flex xs12>
-        <v-data-iterator
-          :items="items"
-          item-key="name"
-          :rows-per-page-items="rowsPerPageItems"
+      <v-flex xs7>
+        <v-data-table
+          v-model="selected"
+          :headers="headers"
+          :items="employees"
           :pagination.sync="pagination"
-          content-tag="v-layout"
-          row
-          wrap
+          select-all
+          item-key="id"
+          class="elevation-1"
         >
-          <template v-slot:item="props">
-            <v-flex xs6 sm6 md6 lg6>
-              <v-card>
-                <v-card-title>
-                  <h4>{{ props.item.name }}</h4>
-                </v-card-title>
-               
-                <v-divider></v-divider>
-                <v-list dense>
-                  <v-list-tile>
-                    <v-list-tile-content>Calories:</v-list-tile-content>
-                    <v-list-tile-content class="align-end">{{ props.item.calories }}</v-list-tile-content>
-                  </v-list-tile>
-                  <v-list-tile>
-                    <v-list-tile-content>Fat:</v-list-tile-content>
-                    <v-list-tile-content class="align-end">{{ props.item.fat }}</v-list-tile-content>
-                  </v-list-tile>
-                  <v-list-tile>
-                    <v-list-tile-content>Carbs:</v-list-tile-content>
-                    <v-list-tile-content class="align-end">{{ props.item.carbs }}</v-list-tile-content>
-                  </v-list-tile>
-                  <v-list-tile>
-                    <v-list-tile-content>Protein:</v-list-tile-content>
-                    <v-list-tile-content class="align-end">{{ props.item.protein }}</v-list-tile-content>
-                  </v-list-tile>
-                  <v-list-tile>
-                    <v-list-tile-content>Sodium:</v-list-tile-content>
-                    <v-list-tile-content class="align-end">{{ props.item.sodium }}</v-list-tile-content>
-                  </v-list-tile>
-                  <v-list-tile>
-                    <v-list-tile-content>Calcium:</v-list-tile-content>
-                    <v-list-tile-content class="align-end">{{ props.item.calcium }}</v-list-tile-content>
-                  </v-list-tile>
-                  <v-list-tile>
-                    <v-list-tile-content>Iron:</v-list-tile-content>
-                    <v-list-tile-content class="align-end">{{ props.item.iron }}</v-list-tile-content>
-                  </v-list-tile>
-                </v-list>
-              </v-card>
-            </v-flex>
+          <template v-slot:headers="props">
+            <tr>
+              <th>
+                <v-checkbox
+                  :input-value="props.all"
+                  :indeterminate="props.indeterminate"
+                  hide-details
+                  @click.stop="toggleAll"
+                ></v-checkbox>
+              </th>
+              <th
+                v-for="header in props.headers"
+                :key="header.text"
+                :class="['text-uppercase column sortable', pagination.descending ? 'desc' : 'asc', header.value === pagination.sortBy ? 'active' : '']"
+                @click="changeSort(header.value)"
+              >
+                <v-icon small>arrow_upward</v-icon>
+                {{ header.text }}
+              </th>
+            </tr>
           </template>
-        </v-data-iterator>
+          <template v-slot:items="props">
+            <tr :active="props.selected" @click="props.selected = !props.selected">
+              <td>
+                <v-checkbox :input-value="props.selected" primary hide-details></v-checkbox>
+              </td>
+              <td class="text-xs-center">{{ props.item.hiringDate }}</td>
+              <td class="text-xs-left font-weight-bold">{{ props.item.name }}</td>
+              <td class="text-xs-leftt font-weight-bold">{{ props.item.surname }}</td>
+              <td class="text-xs-left">{{ props.item.level }}</td>
+              <td class="text-xs-center">{{ props.item.peDate }}</td>
+            </tr>
+          </template>
+        </v-data-table>
       </v-flex>
     </v-layout>
   </v-container>
 </template>
 
 <script>
+let row = {
+  hiringDate: "01.03.2010",
+  name: "Oleksandr",
+  surname: "Storokha",
+  level: "Tech Lead",
+  peDate: "01.03.2010"
+};
+let rows = Array(100).fill(row);
+
+import axios from "axios";
+
 export default {
+  async created() {
+    const res = await axios.get(this.$backendUrl + `api/employees`);
+    this.employees = res.data.map(e => ({
+      ...row,
+      name: e.nameTemp,
+      level: e.employeeType.type,
+      id: e.id
+    }));
+  },
   data: () => ({
     sortByColumns: [
       { value: 0, text: "Active employee" },
       { value: 1, text: "Hiring date" }
     ],
     sortByColumn: 0,
-    rowsPerPageItems: [4, 8, 12],
     pagination: {
-      rowsPerPage: 4
+      sortBy: "name"
     },
-    items: [
+    selected: [],
+    headers: [
       {
-        name: "Frozen Yogurt",
-        calories: 159,
-        fat: 6.0,
-        carbs: 24,
-        protein: 4.0,
-        sodium: 87,
-        calcium: "14%",
-        iron: "1%"
+        text: "Hiring date",
+        value: "hiringDate",
+        sortable: false,
+        align: "right"
       },
-      {
-        name: "Ice cream sandwich",
-        calories: 237,
-        fat: 9.0,
-        carbs: 37,
-        protein: 4.3,
-        sodium: 129,
-        calcium: "8%",
-        iron: "1%"
-      },
-      {
-        name: "Eclair",
-        calories: 262,
-        fat: 16.0,
-        carbs: 23,
-        protein: 6.0,
-        sodium: 337,
-        calcium: "6%",
-        iron: "7%"
-      },
-      {
-        name: "Cupcake",
-        calories: 305,
-        fat: 3.7,
-        carbs: 67,
-        protein: 4.3,
-        sodium: 413,
-        calcium: "3%",
-        iron: "8%"
+      { text: "Name", value: "name" },
+      { text: "Surname", value: "surname" },
+      { text: "Level", value: "level" },
+      { text: "Pe date", value: "peDate" }
+    ],
+    employees: []
+  }),
+  methods: {
+    toggleAll() {
+      if (this.selected.length) this.selected = [];
+      else this.selected = this.employees.slice();
+    },
+    changeSort(column) {
+      if (this.pagination.sortBy === column) {
+        this.pagination.descending = !this.pagination.descending;
+      } else {
+        this.pagination.sortBy = column;
+        this.pagination.descending = false;
       }
-    ]
-  })
+    }
+  }
 };
 </script>
 
