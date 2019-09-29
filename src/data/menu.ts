4df7c23a4b3d5e2121af5cb49f48995807c0b57f
Vitalii Drag <ruban.vitaliy@gmail.com>
2019-09-29 20:19:58 +0300

    Updated roles access to the left menu

--- src/data/menu.ts
+++ src/data/menu.ts
@@ -1,188 +1,145 @@
 import { NavigationGroup, NavigationItem } from "@/models/navigation.interface";
 
-const Menu: Array<NavigationItem> = [
+const sysadminNavigationItems: NavigationItem[] = [
   {
-    title: "Home",
-    icon: "home",
-    router: "/"
+    title: "System settings",
+    icon: "settings_applications",
+    router: "/system",
+    group: NavigationGroup.Personal,
+    accessedBy: ['sysadmin']
   },
-  {
-    title: "Personal",
-    icon: "home",
-    router: "/personal"
-  },
-  {
-    title: "Organization",
-    icon: "supervisor_account",
-    router: "/organization"
-  },
-  {
-    title: "HR",
-    icon: "ballot",
-    router: "/evaluation"
-  },
-  {
-    title: "Administration",
-    icon: "accessible_forward",
-    router: "/administration"
-  },
-  {
-    title: "OKR",
-    icon: "timeline",
-    router: "/okr"
-  },
-  {
-    title: "About",
-    icon: "info",
-    router: "/about"
-  },
+];
+
+const adminNavigationItems: NavigationItem[] = [
   {
     title: "Configuration",
     icon: "build",
     router: "/admin",
-    authRequired: true,
-    adminRoleRequired: true,
-    autoHide: true,
-    group: NavigationGroup.Personal
+    group: NavigationGroup.Personal,
+    accessedBy: ['admin']
   },
   {
     title: "Employees",
     icon: "receipt",
     router: "/administration/employees",
-    authRequired: true,
-    adminRoleRequired: true,
-    autoHide: true,
-    group: NavigationGroup.Personal
+    group: NavigationGroup.Personal,
+    accessedBy: ['admin']
   },
   {
     title: "Projects",
     icon: "receipt",
     router: "/administration/project",
-    authRequired: true,
-    adminRoleRequired: true,
-    autoHide: true,
-    group: NavigationGroup.Personal
-  },
-  {
-    title: "360 Questionarie",
-    icon: "receipt",
-    router: "/administration/360-questionarie",
-    authRequired: true,
-    hrRoleRequired: true,
-    autoHide: true,
-    group: NavigationGroup.Personal
+    group: NavigationGroup.Personal,
+    accessedBy: ['admin']
   },
   {
     title: "Positions Grade",
     icon: "receipt",
     router: "/administration/positions-grade",
-    authRequired: true,
-    adminRoleRequired: true,
-    autoHide: true,
-    group: NavigationGroup.Personal
-  },
-  {
-    title: "Company Strategy",
-    icon: "receipt",
-    router: "/administration/company-strategy",
-    authRequired: true,
-    hrRoleRequired: true,
-    autoHide: true,
-    group: NavigationGroup.Personal
-  },
-  {
-    title: "360 Schedule",
-    icon: "receipt",
-    router: "/administration/360-schedule-setup",
-    authRequired: true,
-    hrRoleRequired: true,
-    autoHide: true,
-    group: NavigationGroup.Personal
+    group: NavigationGroup.Personal,
+    accessedBy: ['admin']
   },
   {
     title: "Organization Structure",
     icon: "receipt",
     router: "/administration/organization-structure",
-    authRequired: true,
-    adminRoleRequired: true,
-    autoHide: true,
-    group: NavigationGroup.Personal
+    group: NavigationGroup.Personal,
+    accessedBy: ['admin']
   },
-  {
-    title: "System settings",
-    icon: "settings_applications",
-    router: "/system",
-    authRequired: true,
-    sysAdminRoleRequired: true,
-    autoHide: true,
-    group: NavigationGroup.Personal
-  },
+];
 
+const managerNavigationItems: NavigationItem[] = [
   {
     title: "Projects",
     icon: "receipt",
     router: "/organization/project",
-    authRequired: true,
-    managerRoleRequired: true,
-    autoHide: true,
-    group: NavigationGroup.Personal
+    group: NavigationGroup.Personal,
+    accessedBy: ['manager']
   },
   {
     title: "360 Team Review",
     icon: "insert_chart_outlined",
     router: "/evaluation/360-team-view",
-    authRequired: true,
-    managerRoleRequired: true,
-    autoHide: true,
-    group: NavigationGroup.Personal
+    group: NavigationGroup.Personal,
+    accessedBy: ['manager']
   },
   {
     title: "360 Analytics",
     icon: "insert_chart_outlined",
     router: "/evaluation/360-analytics",
-    authRequired: true,
-    managerRoleRequired: true,
-    autoHide: true,
-    group: NavigationGroup.Personal
+    group: NavigationGroup.Personal,
+    accessedBy: ['manager']
   },
   {
     title: "Salary Review Plan",
     icon: "local_atm",
     router: "/salary",
-    authRequired: true,
-    managerRoleRequired: true,
-    autoHide: true,
-    group: NavigationGroup.Personal
+    group: NavigationGroup.Personal,
+    accessedBy: ['manager']
+  },
+];
+
+const hrNavigationItems: NavigationItem[] = [
+  {
+    title: "360 Questionarie",
+    icon: "receipt",
+    router: "/administration/360-questionarie",
+    group: NavigationGroup.Personal,
+    accessedBy: ['hr']
   },
   {
-    title: "Education Cubes",
-    icon: "fa-cube",
-    router: "/education",
-    authRequired: true,
-    managerRoleRequired: true,
-    hrRoleRequired: true,
-    autoHide: true,
-    group: NavigationGroup.Personal
+    title: "Company Strategy",
+    icon: "receipt",
+    router: "/administration/company-strategy",
+    group: NavigationGroup.Personal,
+    accessedBy: ['hr']
   },
+  {
+    title: "360 Schedule",
+    icon: "receipt",
+    router: "/administration/360-schedule-setup",
+    group: NavigationGroup.Personal,
+    accessedBy: ['hr']
+  }
+
+];
+
+const defaultNavigationItems: Array<NavigationItem> = [
   {
     title: "My profile",
     icon: "person",
     router: "/personal/profile",
-    group: NavigationGroup.Main
+    group: NavigationGroup.Main,
+    accessedBy: ['hr', 'default', 'manager']
   },
   {
     title: "360",
     icon: "local_play",
     router: "/personal/i-evaluate",
-    group: NavigationGroup.Main
+    group: NavigationGroup.Main,
+    accessedBy: ['hr', 'default', 'manager']
   },
-  { title: "PDP", icon: "book", router: "/personal/pdp", group: NavigationGroup.Main },
+  {
+    title: "PDP",
+    icon: "book",
+    router: "/personal/pdp",
+    group: NavigationGroup.Main,
+    accessedBy: ['hr', 'default', 'manager']
+  },
   {
     title: "Ideas Platform",
     icon: "lightbulb",
     router: "/ideas-platform",
-    group: NavigationGroup.Main
+    group: NavigationGroup.Main,
+    accessedBy: ['hr', 'default', 'manager']
+  },
+  {
+    title: "Education Cubes",
+    icon: "fa-cube",
+    router: "/education",
+    group: NavigationGroup.Personal,
+    accessedBy: ['hr', 'manager']
   }
 ];
 
-export default Menu;
+export default [...defaultNavigationItems, ...sysadminNavigationItems, ...adminNavigationItems, ...hrNavigationItems, ...managerNavigationItems];
--- src/models/navigation.interface.ts
+++ src/models/navigation.interface.ts
@@ -1,20 +1,26 @@
 // Describe router information
 export interface NavigationItem {
-  header?: string,
-  title?: string,
-  icon?: string,
-  router?: string
-  authRequired?: boolean,
-  sysAdminRoleRequired?: boolean,
-  adminRoleRequired?: boolean,
-  managerRoleRequired?: boolean,
-  hrRoleRequired?: boolean,
-  autoHide?: boolean,
-  unauthRequired?: boolean,
+  header?: string;
+  title?: string;
+  icon?: string;
+  router?: string;
+  authRequired?: boolean;
   group?: NavigationGroup;
+  accessedBy: Role[];
 }
 
 export enum NavigationGroup {
   Main,
   Personal
+}
+
+export type Role = 'admin' | 'sysadmin' | 'manager' | 'hr' | 'default';
+
+export type AccessDescriptor = { [key in Role]?: boolean };
+
+export interface RoleMatrix {
+  userIsSysAdmin: boolean;
+  userIsAdmin: boolean;
+  userIsHR: boolean;
+  userIsManager: boolean
 }
